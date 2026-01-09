ca946788026a3aba0815877b9d307e3b59b96684
MontiF <miguelmonteagudoferia@gmail.com>
2026-01-09 11:25:39 +0100

    cambio css

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
     standalone: true,
     imports: [],
     templateUrl: './login.html',
-    styleUrl: '../app.css'
+    styleUrl: './login.css'
 })
 export class LoginComponent {
 

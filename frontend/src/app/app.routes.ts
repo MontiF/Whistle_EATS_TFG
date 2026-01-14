00d79bdc2f65f392d773f852d6ea20093b0da666
MontiF <miguelmonteagudoferia@gmail.com>
2026-01-14 09:38:41 +0100

    no entiendo nada

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'restaurant', component: RestaurantComponent },
     { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
---
+++ frontend/src/app/restaurant/restaurant.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-restaurant',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './restaurant.html',
+    styleUrl: './restaurant.css'
+})
+export class RestaurantComponent { }
